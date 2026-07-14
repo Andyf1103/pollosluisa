4c70936a9f2da00a4888149504fc0b63a4df0add
Andyf1103 <andyf1103michel@gmail.com>
2026-07-13 20:11:06 -0400

    Fix modelo shift

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    use HasFactory;
+    protected $table="shifts";
+
+    protected $fillable = [
+        'hora_entrada',
+        'hora_salida',
+        'dias_descanso'
+    ];
+
+    protected $casts = [
+        'dias_descanso' =>'array',
+    ];
+public function employees()
+{
+    return $this->hasMany(Employee::class, 'shift_id ');
+}
 }
