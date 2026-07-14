60cea6c2d9eeef711ba26281a0d97bd0eb2d8aa1
Andyf1103 <andyf1103michel@gmail.com>
2026-07-13 20:02:43 -0400

    Creacion de migraciones de employee y shift

--- database/migrations/2026_07_14_000008_create_shifts_table.php
+++ database/migrations/2026_07_14_000008_create_shifts_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('shifts', function (Blueprint $table) {
             $table->id();
+            $table->datetime('hora_entrada');
+            $table->datetime('hora_salida');
+            $table->json('dias_descanso')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('shifts');
     }
 };
