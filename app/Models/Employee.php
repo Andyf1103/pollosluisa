deb43ca17f789629be970d92f2220f8b06a80568
Andyf1103 <andyf1103michel@gmail.com>
2026-07-13 21:19:32 -0400

    Creacion de modelo empleado

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'employees';
+
+    protected $fillable = [
+        'ci',
+        'nombre',
+        'email',
+        'rol',          
+        'shift_id'
+    ];
+
+    public function shift()
+    {
+        return $this->belongsTo(Shift::class, 'shift_id');
+    }
+
+    public function getRolNombreAttribute()
+    {
+        $roles = [
+            'admin' => 'Administrador',
+            'cocinero' => 'Cocinero',
+            'mesero' => 'Mesero'
+        ];
+        return $roles[$this->rol] ?? $this->rol;
+    }
+
+    public function scopeRol($query, $rol)
+    {
+        return $query->where('rol', $rol);
+    }
 }
